1ce0c803cc6c492b80ee9e73a121196923aad09b
Saleh Qadan <salehq1989@gmail.com>
2017-01-05 07:26:28 +0000

    added generated weather api key

--- smartmirror.py
+++ smartmirror.py
@@ -16,12 +16,12 @@ from contextlib import contextmanager
 
 LOCALE_LOCK = threading.Lock()
 
-ip = '<IP>'
+ip = '71.56.132.22'
 ui_locale = '' # e.g. 'fr_FR' fro French, '' as default
 time_format = 12 # 12 or 24
 date_format = "%b %d, %Y" # check python doc for strftime() for options
 news_country_code = 'us'
-weather_api_token = '<TOKEN>' # create account at https://darksky.net/dev/
+weather_api_token = 'd09b7d31dd2fdc4836f58993330925a3' # create account at https://darksky.net/dev/
 weather_lang = 'en' # see https://darksky.net/dev/docs/forecast for full list of language parameters values
 weather_unit = 'us' # see https://darksky.net/dev/docs/forecast for full list of unit parameters values
 latitude = None # Set this if IP location lookup does not work for you (must be a string)
@@ -43,19 +43,19 @@ def setlocale(name): #thread proof function to work with locale
 # maps open weather icons to
 # icon reading is not impacted by the 'lang' parameter
 icon_lookup = {
-    'clear-day': "assets/Sun.png",  # clear sky day
-    'wind': "assets/Wind.png",   #wind
-    'cloudy': "assets/Cloud.png",  # cloudy day
-    'partly-cloudy-day': "assets/PartlySunny.png",  # partly cloudy day
-    'rain': "assets/Rain.png",  # rain day
-    'snow': "assets/Snow.png",  # snow day
-    'snow-thin': "assets/Snow.png",  # sleet day
-    'fog': "assets/Haze.png",  # fog day
-    'clear-night': "assets/Moon.png",  # clear sky night
-    'partly-cloudy-night': "assets/PartlyMoon.png",  # scattered clouds night
-    'thunderstorm': "assets/Storm.png",  # thunderstorm
-    'tornado': "assests/Tornado.png",    # tornado
-    'hail': "assests/Hail.png"  # hail
+    'clear-day': "assets/Sun.png",                      # clear sky day
+    'wind': "assets/Wind.png",                          # wind
+    'cloudy': "assets/Cloud.png",                       # cloudy day
+    'partly-cloudy-day': "assets/PartlySunny.png",      # partly cloudy day
+    'rain': "assets/Rain.png",                          # rain day
+    'snow': "assets/Snow.png",                          # snow day
+    'snow-thin': "assets/Snow.png",                     # sleet day
+    'fog': "assets/Haze.png",                           # fog day
+    'clear-night': "assets/Moon.png",                   # clear sky night
+    'partly-cloudy-night': "assets/PartlyMoon.png",     # scattered clouds night
+    'thunderstorm': "assets/Storm.png",                 # thunderstorm
+    'tornado': "assests/Tornado.png",                   # tornado
+    'hail': "assests/Hail.png"                          # hail
 }
 
 
